3fbc54262d4b6d37f2f3be463736a2875600493a
Arnal <arnal26@hotmail.com>
2020-05-04 20:55:12 -0300

    titule

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -6,9 +6,7 @@ import { AccountService } from 'app/core/auth/account.service';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { LoginService } from 'app/core/login/login.service';
 import { ProfileService } from 'app/layouts/profiles/profile.service';
-import { IRole } from 'app/shared/model/role.model';
-import { RoleService } from '../../entities/role_user/role.service_user';
-import { HttpResponse } from '@angular/common/http';
+
 @Component({
   selector: 'jhi-navbar',
   templateUrl: './navbar.component.html',
@@ -21,31 +19,22 @@ export class NavbarComponent implements OnInit {
   version: string;
   account: any;
   authSubscription?: Subscription;
-  roles?: IRole[];
-
+  eventSubscriber?: Subscription;
   constructor(
     private loginService: LoginService,
     private accountService: AccountService,
     private loginModalService: LoginModalService,
     private profileService: ProfileService,
-    protected roleService: RoleService,
     private router: Router
   ) {
     this.version = VERSION ? (VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION) : '';
   }
-  loadAllrole(): void {
-    this.roleService.query().subscribe((res: HttpResponse<IRole[]>) => (this.roles = res.body || []));
-  }
   ngOnInit(): void {
-    this.loadAllrole;
     this.profileService.getProfileInfo().subscribe(profileInfo => {
       this.inProduction = profileInfo.inProduction;
       this.swaggerEnabled = profileInfo.swaggerEnabled;
     });
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
-  }
-  filterdonor(login: String): any{
-    this.roles?.filter(x=>x.user?.login===login)
   }
   collapseNavbar(): void {
     this.isNavbarCollapsed = true;
@@ -68,15 +57,7 @@ export class NavbarComponent implements OnInit {
   toggleNavbar(): void {
     this.isNavbarCollapsed = !this.isNavbarCollapsed;
   }
-  trackId(index: number, item: IRole): number {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-    return item.id!;
-  }
-  filter(login: String): any {
-    return this.roles?.filter(x=>x.user?.login===login)
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 
-  }
   getImageUrl(): string {
     return this.isAuthenticated() ? this.accountService.getImageUrl() : '';
   }
